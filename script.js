3ab791714a77d5d9896f509fe098cfeb362bde6f
Mega <syedarbabalishah@gmail.com>
2024-09-20 05:17:10 -0700

    Extension

---
+++ script.js
@@ -0,0 +1,12 @@
+async function fetchData() {
+    const res=await fetch ("https://openexchangerates.org/api/latest.json?app_id=acb29aad06f94cd19c4235fff36edcad");
+    const record=await res.json();
+    document.getElementById("PKR").innerHTML=record.rates["PKR"];
+    document.getElementById("USD").innerHTML=record.rates["USD"];
+    document.getElementById("GBP").innerHTML=record.rates["GBP"];
+    document.getElementById("AED").innerHTML=record.rates["AED"];
+    // document.getElementById("areaName").innerHTML=record.data[0].areaName;
+    // document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
+    // document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+}
+fetchData();
